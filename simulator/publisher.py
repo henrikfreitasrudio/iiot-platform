6c51fe12402ef949651d13be70be5806ac92e0f9
Henrik de Freitas <henrik_freitas2@outlook.com>
2026-04-18 13:27:17 -0300

    feat: configurar Docker e implementar publisher MQTT

--- simulator/publisher.py
+++ simulator/publisher.py
@@ -1,14 +1,15 @@
 import time, json, random
 import paho.mqtt.client as mqtt
 
-client = mqtt.Client()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect("mosquitto", 1883)
 
 while True:
     data = {
-        "device": "esp32_01",
-        "temperature": random.uniform(20,30),
-        "pressure": random.uniform(1, 2)
+        "device": "sim_esp32",
+        "temperature": 25 + (time.time() % 5),
+        "pressure": 60 + (time.time() % 10),
+        "timestamp": time.time()
     }
     client.publish("sensors/data", json.dumps(data))
     print(data)
