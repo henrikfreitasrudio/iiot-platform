e7d0e74a751b3688d9d7fadfe3fa0082fecc0a5b
Henrik de Freitas <henrik_freitas2@outlook.com>
2026-04-20 10:14:50 -0300

    feat: persist sensor data in influxdb

--- worker/consumer.py
+++ worker/consumer.py
@@ -1,5 +1,16 @@
 import json
+from datetime import datetime
 import paho.mqtt.client as mqtt
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+
+# Conexão Influx
+client_influx = InfluxDBClient(
+    url="http://influxdb:8086",
+    token="token123",
+    org="iiot"
+)
+
+write_api = client_influx.write_api()
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print("Connected to MQTT", reason_code)
@@ -8,6 +19,17 @@ def on_connect(client, userdata, flags, reason_code, properties):
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload.decode())
     print("Received: ", data, flush=True)
+    
+    device = data.get("device_id" or data.get("device") or "unknown")
+    point = (
+        Point("sensor_data")
+        .tag("device", data["device"])
+        .field("temperature", float(data["temperature"]))
+        .field("pressure", float(data["pressure"]))
+        # .time(datetime.utcnow(), WritePrecision.NS)
+    )
+    
+    write_api.write(bucket="iiot", org="iiot", record=point)
     
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
